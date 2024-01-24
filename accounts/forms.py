28b7f03fdf2643dae54131675f439bbed264a554
pedram-macbook <pedram@email.com>
2024-01-24 18:06:58 +0330

    add form to accounts register view and add templates for show form

--- accounts/forms.py
+++ accounts/forms.py
@@ -36,6 +36,7 @@ class UserChangeForm(forms.ModelForm):
 
 class UserRegistrationForm(forms.Form):
     phone_number = forms.CharField(max_length=11, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailField(attrs={'class': 'form-class'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     full_name = forms.CharField(max_length=55, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
 
 class UserRegisterView(View):
-    def get(self , request):
-        return render(request , 'accounts/register.html')
+    form_class = UserRegistrationForm
+    template_name = 'accounts/register.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        pass
